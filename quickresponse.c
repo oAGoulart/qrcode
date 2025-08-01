18baaf4744f929e6058a4ef777271744cf35f4fa
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2025-08-01 07:22:05 -0300

    Add more verbose information
    Signed-off-by: Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>

--- quickresponse.c
+++ quickresponse.c
@@ -14,7 +14,7 @@ extern const uint8_t logt[];
 extern const uint8_t alogt[];
 extern const uint8_t rsgen[];
 
-static void
+static __inline__ void
 vshift_(uint8_t* v, uint8_t length)
 {
   uint8_t ui8 = 0;
@@ -35,7 +35,7 @@ struct qrcode_s
 };
 
 int
-create_qrcode(qrcode_t** self, char* str, uint8_t should_debug)
+create_qrcode(qrcode_t** self, char* str, uint8_t verbose)
 {
   const uint8_t bitmask[8] = {1u, 2u, 4u, 8u, 16u, 32u, 64u, 128u};
   const uint8_t strmax[MAX_VERSION] = {17u, 32u, 53u, 78u, 106u};
@@ -46,8 +46,8 @@ create_qrcode(qrcode_t** self, char* str, uint8_t should_debug)
   {
     return EINVAL;
   }
-  size_t str_count = strlen(str);
-  if (str_count > strmax[MAX_VERSION - 1])
+  size_t strcount = strlen(str);
+  if (strcount > strmax[MAX_VERSION - 1])
   {
     fprintf(stderr, "\tstring must be less than %u characters long\r\n",
             strmax[MAX_VERSION - 1]);
@@ -62,37 +62,43 @@ create_qrcode(qrcode_t** self, char* str, uint8_t should_debug)
   uint8_t version = 0;
   for (; version < MAX_VERSION; version++)
   {
-    if (str_count <= strmax[version])
+    if (strcount <= strmax[version])
     {
       break;
     }
     offset += ecclen[version] + 1;
   }
   const uint8_t* gen = rsgen + offset;
+  if (verbose)
+  {
+    printf("(INFO) String length: %u\r\n"
+           "(INFO) Version selected: %u\r\n",
+           (uint32_t)strcount, version + 1);
+  }
   (*self)->version_ = version;
-  const uint8_t data_len = strmax[version] + 2;
-  (*self)->slen_ = data_len;
-  (*self)->stream_ = (uint8_t*)malloc(data_len);
+  const uint8_t datalen = strmax[version] + 2;
+  (*self)->slen_ = datalen;
+  (*self)->stream_ = (uint8_t*)malloc(datalen);
   if ((*self)->stream_ == NULL)
   {
     free(*self);
     *self = NULL;
     return ENOMEM;
   }
-  memset((*self)->stream_, 0, data_len);
-  (*self)->stream_[0] = (GEN_MODE << 4) | (uint8_t)(str_count >> 4);
-  (*self)->stream_[1] = (uint8_t)(str_count << 4);
+  memset((*self)->stream_, 0, datalen);
+  (*self)->stream_[0] = (GEN_MODE << 4) | (uint8_t)(strcount >> 4);
+  (*self)->stream_[1] = (uint8_t)(strcount << 4);
   uint8_t ui8 = 0;
-  for (; ui8 < str_count; ui8++)
+  for (; ui8 < strcount; ui8++)
   {
     (*self)->stream_[ui8 + 1] |= (uint8_t)(str[ui8] >> 4);
     (*self)->stream_[ui8 + 2] = (uint8_t)(str[ui8] << 4);
   }
 
-  uint8_t ecc[data_len];
-  memcpy(&ecc[0], (*self)->stream_, data_len);
+  uint8_t ecc[datalen];
+  memcpy(&ecc[0], (*self)->stream_, datalen);
   // NOTE: polynomial division (long division)
-  for (ui8 = 0; ui8 < data_len; ui8++)
+  for (ui8 = 0; ui8 < datalen; ui8++)
   {
     uint8_t lead = ecc[0];
     uint8_t uj8 = 0;
@@ -100,28 +106,28 @@ create_qrcode(qrcode_t** self, char* str, uint8_t should_debug)
     {
       ecc[uj8] ^= alogt[(gen[uj8] + logt[lead]) % GF_MAX];
     }
-    vshift_(&ecc[0], data_len);
+    vshift_(&ecc[0], datalen);
   }
-  const uint8_t total_bytes = numbytes[version];
-  uint8_t* tmp_ptr = (uint8_t*)realloc((*self)->stream_, total_bytes);
-  if (tmp_ptr == NULL)
+  const uint8_t byteslen = numbytes[version];
+  uint8_t* tmpptr = (uint8_t*)realloc((*self)->stream_, byteslen);
+  if (tmpptr == NULL)
   {
     free((*self)->stream_);
     free(*self);
     *self = NULL;
     return ENOMEM;
   }
-  (*self)->stream_ = tmp_ptr;
-  memcpy(&(*self)->stream_[data_len], &ecc[0], ecclen[version]);
-  if (should_debug)
+  (*self)->stream_ = tmpptr;
+  memcpy(&(*self)->stream_[datalen], &ecc[0], ecclen[version]);
+  if (verbose)
   {
-    printf("(INFO) Calculated bytes (%d): [0x%x",
-           total_bytes, (*self)->stream_[0]);
-    for (ui8 = 1; ui8 < total_bytes; ui8++)
+    printf("(INFO) Calculated bytes (%u): [ 0x%x",
+           byteslen, (*self)->stream_[0]);
+    for (ui8 = 1; ui8 < byteslen; ui8++)
     {
       printf(", 0x%x", (*self)->stream_[ui8]);
     }
-    puts("]");
+    puts(" ]");
   }
 
   for (ui8 = 0; ui8 < NUM_MASKS; ui8++)
@@ -133,7 +139,7 @@ create_qrcode(qrcode_t** self, char* str, uint8_t should_debug)
       return ENOMEM;
     }
   }
-  for (ui8 = 0; ui8 < total_bytes; ui8++)
+  for (ui8 = 0; ui8 < byteslen; ui8++)
   {
     offset = ui8 * 8;
     // NOTE: bitstream goes from bit 7 to bit 0
@@ -157,32 +163,32 @@ create_qrcode(qrcode_t** self, char* str, uint8_t should_debug)
       uint8_t uj8 = 0;
       for (; uj8 < NUM_MASKS; uj8++)
       {
-        uint16_t index = (uint16_t)(total_bytes * 8) + ui8;
+        uint16_t index = (uint16_t)(byteslen * 8) + ui8;
         qrmask_set((*self)->masks_[uj8], index, MASK_LIGHT);
       }
     }
   }
-  uint16_t curr_score = 0;
-  uint16_t min_score = UINT16_MAX;
+  uint16_t score = 0;
+  uint16_t minscore = UINT16_MAX;
   uint8_t chosen = 0;
   for (ui8 = 0; ui8 < NUM_MASKS; ui8++)
   {
-    curr_score = qrmask_penalty((*self)->masks_[ui8]);
+    score = qrmask_penalty((*self)->masks_[ui8]);
     qrmask_apply((*self)->masks_[ui8]);
-    if (curr_score < min_score)
+    if (score < minscore)
     {
-      min_score = curr_score;
+      minscore = score;
       chosen = ui8;
     }
-    if (should_debug)
+    if (verbose)
     {
-      printf("(INFO) Mask [%d] penalty: %d\r\n", ui8, curr_score);
+      printf("(INFO) Mask [%u] penalty: %u\r\n", ui8, score);
     }
   }
   (*self)->chosen_ = chosen;
-  if (should_debug)
+  if (verbose)
   {
-    printf("(INFO) Mask chosen: %d\r\n", chosen);
+    printf("(INFO) Mask chosen: %u\r\n", chosen);
   }
   return 0;
 }
@@ -210,16 +216,15 @@ delete_qrcode(qrcode_t** self)
 }
 
 void
-qrcode_print(qrcode_t* self, uint8_t use_raw, int force_mask)
+qrcode_print(qrcode_t* self, uint8_t useraw, int mask)
 {
-  uint8_t mask = (force_mask >= 0 && force_mask < 8) ?
-    (uint8_t)force_mask : self->chosen_;
-  if (use_raw)
+  uint8_t m = (mask >= 0 && mask < 8) ? (uint8_t)mask : self->chosen_;
+  if (useraw)
   {
-    qrmask_raw(self->masks_[mask]);
+    qrmask_raw(self->masks_[m]);
   }
   else
   {
-    qrmask_print(self->masks_[mask]);
+    qrmask_print(self->masks_[m]);
   }
 }
--- quickresponse.h
+++ quickresponse.h
@@ -6,11 +6,11 @@
 typedef struct qrcode_s qrcode_t;
 
 __attribute__((__nonnull__)) int
-create_qrcode(qrcode_t** self, char* str, uint8_t should_debug);
+create_qrcode(qrcode_t** self, char* str, uint8_t verbose);
 __attribute__((__nonnull__)) void
 delete_qrcode(qrcode_t** self);
 
 __attribute__((__nonnull__)) void
-qrcode_print(qrcode_t* self, uint8_t use_raw, int force_mask);
+qrcode_print(qrcode_t* self, uint8_t useraw, int mask);
 
 #endif
