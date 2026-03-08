8558092b901f78a63daf2b991ac3f413b9115da9
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2026-03-08 18:19:40 -0300

    Fix Unix build

--- scripts/lookup.sh
+++ scripts/lookup.sh
@@ -9,7 +9,7 @@ generate_indexes() {
   echo "[2/3] Generating align pattern indexes table"
   for ((i = 2 ; i <= MAX_VERSION ; i++)); do
     printf "\nglobale_ sym_(%s_%i)\n" "qralign" "$i" >> "$OUT_FILE"
-    python "$SCRIPT_DIR"/indexes.py "$i" align >> "$OUT_FILE"
+    python3 "$SCRIPT_DIR"/indexes.py "$i" align >> "$OUT_FILE"
   done
   printf "\nglobale_ sym_(%s)\n" "qralign" >> "$OUT_FILE"
   for ((i = 2 ; i <= MAX_VERSION ; i++)); do
@@ -19,7 +19,7 @@ generate_indexes() {
   echo "[3/3] Generating module indexes table"
   for ((i = 1 ; i <= MAX_VERSION ; i++)); do
     printf "\nglobale_ sym_(%s_%i)\n" "qrindex" "$i" >> "$OUT_FILE"
-    python "$SCRIPT_DIR"/indexes.py "$i" >> "$OUT_FILE"
+    python3 "$SCRIPT_DIR"/indexes.py "$i" >> "$OUT_FILE"
   done
   printf "\nglobale_ sym_(%s)\n" "qrindex" >> "$OUT_FILE"
   for ((i = 1 ; i <= MAX_VERSION ; i++)); do
@@ -30,7 +30,7 @@ generate_indexes() {
 generate_info() {
   echo "[1/3] Generating version info table"
   printf "\nglobale_ sym_(%s)\n" "qrinfo" >> "$OUT_FILE"
-  python "$SCRIPT_DIR"/info.py "$SCRIPT_DIR"/"$CSV_FILE" >> "$OUT_FILE"
+  python3 "$SCRIPT_DIR"/info.py "$SCRIPT_DIR"/"$CSV_FILE" >> "$OUT_FILE"
 }
 
 create_lookup() {
