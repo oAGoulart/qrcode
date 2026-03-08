70faf2b229d0079cc14c66c26b4149e045aa6297
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2026-03-08 20:50:44 -0300

    Change LUTs build script to separate data and pointers

--- scripts/lookup.sh
+++ scripts/lookup.sh
@@ -5,38 +5,64 @@ CSV_FILE="qrinfo.csv"
 SCRIPT_DIR="$(dirname "$0")"
 MAX_VERSION=40
 
-generate_indexes() {
-  echo "[2/3] Generating align pattern indexes table"
+generate_info() {
+  echo "[1/4] Generating version info table"
+  printf "\nglobale_ sym_(%s)\n" "qrinfo" >> "$OUT_FILE"
+  python3 "$SCRIPT_DIR"/info.py "$SCRIPT_DIR"/"$CSV_FILE" >> "$OUT_FILE"
+}
+
+generate_indexes_data() {
+  echo "[2/4] Generating align pattern indexes data"
   for ((i = 2 ; i <= MAX_VERSION ; i++)); do
     printf "\nglobale_ sym_(%s_%i)\n" "qralign" "$i" >> "$OUT_FILE"
     python3 "$SCRIPT_DIR"/indexes.py "$i" align >> "$OUT_FILE"
   done
+
+  echo "[3/4] Generating module indexes data"
+  for ((i = 1 ; i <= MAX_VERSION ; i++)); do
+    printf "\nglobale_ sym_(%s_%i)\n" "qrindex" "$i" >> "$OUT_FILE"
+    python3 "$SCRIPT_DIR"/indexes.py "$i" >> "$OUT_FILE"
+  done
+}
+
+generate_pointers() {
+  echo "[4/4] Generating pointer tables"
+  cat << 'EOF' >> "$OUT_FILE"
+
+#if defined(_WIN32) || defined(__MSYS__) || defined(__CYGWIN__)
+  .section .data,"dw"
+#elif defined(__APPLE__)
+  .section __DATA,__const
+#else
+  .section .data.rel.ro,"aw",@progbits
+#endif
+EOF
+
   printf "\nglobale_ sym_(%s)\n" "qralign" >> "$OUT_FILE"
   for ((i = 2 ; i <= MAX_VERSION ; i++)); do
     printf "  pointer_ sym_(%s_%i)\n" "qralign" "$i" >> "$OUT_FILE"
   done
 
-  echo "[3/3] Generating module indexes table"
-  for ((i = 1 ; i <= MAX_VERSION ; i++)); do
-    printf "\nglobale_ sym_(%s_%i)\n" "qrindex" "$i" >> "$OUT_FILE"
-    python3 "$SCRIPT_DIR"/indexes.py "$i" >> "$OUT_FILE"
-  done
   printf "\nglobale_ sym_(%s)\n" "qrindex" >> "$OUT_FILE"
   for ((i = 1 ; i <= MAX_VERSION ; i++)); do
     printf "  pointer_ sym_(%s_%i)\n" "qrindex" "$i" >> "$OUT_FILE"
   done
 }
 
-generate_info() {
-  echo "[1/3] Generating version info table"
-  printf "\nglobale_ sym_(%s)\n" "qrinfo" >> "$OUT_FILE"
-  python3 "$SCRIPT_DIR"/info.py "$SCRIPT_DIR"/"$CSV_FILE" >> "$OUT_FILE"
-}
-
 create_lookup() {
-  cp "$SCRIPT_DIR"/lookup.S "$OUT_FILE"
+  cp "$SCRIPT_DIR"/lookup.S "$OUT_FILE" 
+
   generate_info
-  generate_indexes
+  generate_indexes_data
+  generate_pointers
+
+  cat << 'EOF' >> "$OUT_FILE"
+
+#if defined(__APPLE__)
+  .subsections_via_symbols
+#endif
+EOF
+
 }
 
 create_lookup
